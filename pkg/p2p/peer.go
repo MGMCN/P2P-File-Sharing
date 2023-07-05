b809902cc3ffb1523a7ac098ddeecd4e92f27ca1
Takayama <49364055+MGMCN@users.noreply.github.com>
2023-07-05 21:18:32 +0900

    remove unnecessary code

--- pkg/p2p/peer.go
+++ pkg/p2p/peer.go
@@ -132,24 +132,6 @@ func (p *p2pNode) startCommandExecutor() {
 	for command := range p.commandChan {
 		commands := strings.Split(command, " ")
 		if len(commands) >= 2 {
-			//if commands[0] == "cache" {
-			//	p.cli.ExecuteCommand(commands)
-			//} else {
-			//	// Not graceful
-			//	if commands[0] == "peer" && len(commands) >= 2 {
-			//		senderHandler := p.handlerManager.GetSenderHandler(commands[1])
-			//		if senderHandler != nil {
-			//			go func() {
-			//				errs := senderHandler.OpenStreamAndSendRequest(commands)
-			//				if len(errs) != 0 {
-			//					log.Printf("Some errors occurred while executing %s\n", commands)
-			//				}
-			//			}()
-			//		}
-			//	} else {
-			//		log.Println("Missing parameters")
-			//	}
-			//}
 			p.cli.Execute(commands, p.handlerManager.GetSenderHandler(commands[1]))
 		} else {
 			log.Println("Missing parameters")
